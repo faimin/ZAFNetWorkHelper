978845e65e7f4e3804342c3209dcc54791d4da5f
符现超 <fuxianchao@gmail.com>
2016-10-19 20:03:37 +0800

    add test

--- Demo/DemoTests/DemoTests.m
+++ Demo/DemoTests/DemoTests.m
@@ -42,29 +42,40 @@
     
 }
 
+// http://www.tuicool.com/articles/jUrqiqR
 - (void)testAsyncExample {
     NSString *testURL = @"http://baike.baidu.com/api/openapi/BaikeLemmaCardApi?scope=103&format=json&appid=379020&bk_key=贾静雯&bk_length=600";
-    XCTestExpectation *exp = [self expectationWithDescription:@"出错原因~~~"];
+    XCTestExpectation *exp = [self expectationWithDescription:@"保佑测试成功"];
     
     NSOperationQueue *zd_queue = [[NSOperationQueue alloc] init];
     [zd_queue addOperationWithBlock:^{
-        [[ZDNetworkHelper shareInstance] requestWithURL:testURL params:nil httpMethod:HttpMethod_GET progress:^(NSProgress * _Nonnull progress, CGFloat progressValue) {
-            NSLog(@"进度 == %0.1f", progressValue);
-        } success:^(id  _Nullable responseObject) {
-            NSLog(@"请求成功===> %@", responseObject);
-            // 此处类似于信号量中的发送信号或者promise中的完成事件
-            [exp fulfill];
-        } failure:^(NSError * _Nonnull error) {
-            NSLog(@"请求失败---> %@", error);
-        }];
+        
     }];
     
-    // 超出时间限制后报错，测试不通过
-    [self waitForExpectationsWithTimeout:5 handler:^(NSError * _Nullable error) {
-        if (error) {
-            NSLog(@"-----> %@", error);
-        }
+    NSURLSessionDataTask *task1 = [[ZDNetworkHelper shareInstance] requestWithURL:testURL params:nil httpMethod:HttpMethod_GET progress:^(NSProgress * _Nonnull progress, CGFloat progressValue) {
+        NSLog(@"进度 == %0.1f", progressValue);
+    } success:^(id  _Nullable responseObject) {
+        NSLog(@"请求成功===> %@", responseObject);
+        // 宣布满足条件（此处类似于信号量中的发送信号或者promise中的完成事件）
+        [exp fulfill];
+    } failure:^(NSError * _Nonnull error) {
+        NSLog(@"请求失败---> %@", error);
     }];
+    
+    XCTAssertNotNil(task1);
+    
+    NSPredicate *predicate = [NSPredicate predicateWithBlock:^BOOL(id  _Nullable evaluatedObject, NSDictionary<NSString *,id> * _Nullable bindings) {
+        return task1.state == NSURLSessionTaskStateRunning;
+    }];
+    [self expectationForPredicate:predicate evaluatedWithObject:task1 handler:nil];
+    
+    
+    // 延迟多少秒后，如果没有满足条件就报错
+    [self waitForExpectationsWithTimeout:5 handler:^(NSError * _Nullable error) {
+        if (error) NSLog(@"-----> %@", error);
+    }];
+    
+    XCTAssertTrue(task1.state == NSURLSessionTaskStateCompleted);
 }
 
 @end
